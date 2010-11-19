ac28e9df29b5b947cc1b966f881fbcc03263bfd1
Jonathan Chiu <jscchiu@gmail.com>
2010-11-18 23:28:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grabbit::Application.config.secret_token = '1df1d774973a21b8cb9e295a5fcf4b8c9265d824ad3a3ca233c2f42467bdba4c13987703b9e37f718f2baf2e0216f35a8e59cf14d9cad9b6a93ed03a42daff42'
